992b6c8c4c36abaeb8fb3f8b67d5c7047d84b84c
miguel <miguelvilag@gmail.com>
2016-10-12 15:28:10 -0400

    modal for selecting spreadsheet or creating a new one

--- client/src/main/scala/spreadsheet/Main.scala
+++ client/src/main/scala/spreadsheet/Main.scala
@@ -19,23 +19,72 @@ object Main extends js.JSApp {
 
     val modalId ="spreadsheet-names"
     val modalsElem = document.getElementById("modals")
-    val content = document.createElement("div")
-    val modal = Modal(modalId, "Create new spreadsheet or select an existing one") {
-      content
+    val spreadSheetList = document.createElement("div")
+
+    def openSpreadsheet(name: String): Unit = {
+      val cellsElem = document.getElementById("cells")
+      println(s"OPENING: $name")
+      val connection = new SpreadsheetConnection(cellsElem, name)
     }
+
+    val modal: Element = Modal(modalId, "Create a new spreadsheet or select an existing one") {
+      val root = document.createElement("div")
+      val listPanel1 = document.createElement("div")
+      listPanel1.classList.add("panel")
+      listPanel1.classList.add("panel-default")
+      listPanel1 appendChild {
+        val panelBody = document.createElement("div")
+        panelBody.classList.add("panel-body")
+        panelBody.appendChild(spreadSheetList)
+        panelBody
+      }
+      root.appendChild(listPanel1)
+      val listPanel2 = document.createElement("div")
+      listPanel2.classList.add("panel")
+      listPanel2.classList.add("panel-default")
+      listPanel2.appendChild {
+        val form = document.createElement("form")
+        import org.scalajs.dom.raw.HTMLInputElement
+        val input = document.createElement("input").asInstanceOf[HTMLInputElement]
+        input.setAttribute("type","text")
+        input.setAttribute("placeholder","Name")
+        input.classList.add("form-control")
+        form.appendChild {
+          input
+        }
+        form.appendChild {
+          import org.scalajs.dom.raw.HTMLButtonElement
+          val button = document.createElement("button").asInstanceOf[HTMLButtonElement]
+          button.setAttribute("type","submit")
+          button.classList.add("btn")
+          button.classList.add("btn-default")
+          button.appendChild(document.createTextNode("Create new spreadsheet"))
+          button.addEventListener("click", { _: Event =>
+                                    jQuery(s"#$modalId").modal("hide")
+                                    openSpreadsheet(input.value.replaceAll(" +", "-"))
+                                  })
+          button
+        }
+        form
+      }
+      root.appendChild(listPanel2)
+
+      root
+    }
+
     modalsElem.appendChild(modal)
     jQuery(modal).modal("show")
 
     HttpRequest(url).send().onComplete {
       case res:Success[SimpleHttpResponse] =>
         val spreadsheetNames = read[List[String]](res.get.body)
+        println(s"spreadsheetNames = $spreadsheetNames")
         spreadsheetNames foreach { name =>
           val button = createButton(name, onClick = { () =>
-            jQuery(modal).modal("hide")
-            val cellsElem = document.getElementById("cells")
-            val connection = new SpreadsheetConnection(cellsElem, name)
+                                      jQuery(modal).modal("hide")
+                                      openSpreadsheet(name)
           })
-          content.appendChild(button)
+          spreadSheetList.appendChild(button)
         }
       case e: Failure[SimpleHttpResponse] => println("Fuuuuck!")
 
--- server/src/main/scala/spreadsheet/SpreadsheetWebsocket.scala
+++ server/src/main/scala/spreadsheet/SpreadsheetWebsocket.scala
@@ -1,134 +1,24 @@
 package spreadsheet
 
 import org.http4s._
-import org.http4s.server.ServerApp
-import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.websocket.WebsocketBits._
 import org.http4s.dsl._
 import org.http4s.server.websocket._
 import org.http4s.server.websocket.WS
 
-import scalaz.concurrent.Task
-import scalaz.concurrent.Strategy
-import scalaz.stream.async.unboundedQueue
-import scalaz.stream.{DefaultScheduler, Exchange}
-import scalaz.stream.time.awakeEvery
-
-import scala.collection.mutable.HashMap
-
-import scalaz.stream.{Exchange, Process, time}
-import scalaz.stream.async.topic
-import scalaz.syntax.either._
-import scalaz.{\/,-\/,\/-}
-import scalaz.concurrent.Task
-
-import org.log4s.getLogger
-import upickle.legacy._
-
-import woot._
-
-import java.nio.file.{Paths, Files}
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ Await, ExecutionContext , Future }
-import scala.util.{ Success, Failure }
-import spreadsheet.serialization.Writers._
-import spreadsheet.serialization.Readers.spreadSheetReader
-
-import reactivemongo.api.{ DefaultDB, MongoConnection, MongoDriver }
-import reactivemongo.bson.{ document, BSONBoolean, BSONDocument }
-
 class SpreadsheetWebsocket {
 
-  val spColl = MongoDB.spColl
-
-  var sp: SpreadSheetContent = null
-
-  def saveToMongo(): Task[Unit] = toTask {
-    spColl.flatMap(_.update(document("name" -> sp.name), sp, upsert = true)).map(_ => ())
-  }
-
-  def toTask[A](future: => Future[A])(implicit ec: ExecutionContext): Task[A] = {
-    Task.async { callback =>
-      future.onComplete {
-        case Success(value) => callback(value.right)
-        case Failure(error) => callback(error.left )
-      }
-    }
-  }
-
-  def readFromMongo(name: String): Future[Option[SpreadSheetContent]] = {
-    spColl.flatMap(_.find(document("name" -> name)).one[SpreadSheetContent])
-  }
-
-  private val ops = topic[SpreadSheetOp]()
-
-  def encodeOp(op: SpreadSheetOp): Text = Text(write(ClientMessage(None, Some(op))))
-
-  def updateSpreadSheet(op: SpreadSheetOp): Task[Throwable \/ SpreadSheetOp] = {
-    sp.integrateOperation(op)
-    saveToMongo().map { _ =>
-      op.right
-    }
-  }
-
-  def errorHandler(err: Throwable): Task[Unit] = {
-    println(s"Failed to consume message: $err")
-    Task.fail(err)
-  }
-
-  def parse(json: String): Throwable \/ SpreadSheetOp =
-    \/.fromTryCatchNonFatal { read[SpreadSheetOp](json) }
-
-  def throwError[A](error: Throwable): Task[Throwable \/ A] = Task.now(error.left)
-
-  def decodeFrame(frame: WebSocketFrame): Task[Throwable \/ SpreadSheetOp] =
-    frame match {
-      case Text(json, _) =>
-        parse(json).fold(throwError, updateSpreadSheet)
-      case nonText       =>
-        Task.now(new IllegalArgumentException(s"Cannot handle: $nonText").left)
-    }
-
-  def getSpreadsheet(name: String): SpreadSheetContent = {
-    if(sp != null) {
-      sp
-    } else {
-      val fut = readFromMongo(name).map {
-        case Some(_sp) =>
-          sp = _sp
-          sp
-        case None =>
-          sp = SpreadSheetContent(SiteId("server"), name, 13, 10)
-          sp
-      }
-      Await.result(fut, 500.millis)
-    }
-  }
-
-  def safeConsume(consume: SpreadSheetOp => Task[Unit]): WebSocketFrame => Task[Unit] =
-         ws => decodeFrame(ws).flatMap(_.fold(errorHandler, consume))
+  private var spreadsheets = Map.empty[String, SpreadsheetState]
 
   val route = HttpService {
-    case req@ GET -> Root / "edit" / name =>
-      val clientId = SiteId.random
-
-      getSpreadsheet(name)
-
-      val clientCopy = sp.withSiteId(clientId)
-      val clientCopyJson = write(ClientMessage(Some(clientCopy), None))
-
-      val otherUserOps = ops.subscribe//.filter(_.from != clientId)
-      val src = Process.emit( Text(clientCopyJson) ) ++ otherUserOps.map(encodeOp)
-
-      val snk = ops.publish.map(safeConsume).onComplete(cleanup)
-
-      WS(Exchange(src, snk))
-  }
-
-  private def cleanup() = {
-    println(s"Client disconnect")
-    Process.halt
+    case req @ GET -> Root / "edit" / name =>
+      val sp = spreadsheets.get(name).getOrElse {
+        println(s"CREATING NEW SP: $name")
+        val newSp = new SpreadsheetState(name)
+        spreadsheets = spreadsheets.updated(name, newSp)
+        newSp
+      }
+      sp.createWebsocketConnection()
   }
 
 }
